252df531b4d8051f3a006b5923e3946adb216d05
Rishi Rana <ranarishi298@gmail.com>
2025-09-20 12:47:18 +0530

    Added README.md

--- Backened Test Submission/Controller/url.js
+++ Backened Test Submission/Controller/url.js
@@ -10,11 +10,19 @@ export const getURL = async (req, res) => {
     const shortcode = data.shortcode;
     const validity = data.validity;
     
-    let now = new Date();
-    let futureTime 
-    = new Date(now.getTime() + 30 * 60 * 1000);
+    let now = new Date(); // Get current date and time
 
-    const expiry = futureTime;
+    // add validity 30 min here
+    now.setMinutes(now.getMinutes() + validity); 
+    let year = now.getFullYear().toString().slice(-2);
+    let month = (now.getMonth() + 1).toString().padStart(2, '0');
+    let day = now.getDate().toString().padStart(2, '0');
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+    let formattedDateTime = `${year}-${month}-${day} ${hours}-${minutes}-${seconds}`;
+
+    const expiry = formattedDateTime;
     const shortURL = `http://localhost:3000/${shortcode}`;
 
     res.json({
---
+++ README.md
@@ -0,0 +1,5 @@
+# Backened Project - URL Shortner
+
+![Postman Image](/Postman.png)
+
+- Technologies Used : ExpressJS, NodeJSand Postman for Backened
